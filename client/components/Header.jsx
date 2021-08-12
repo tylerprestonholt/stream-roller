97b37338ac53e4328721b2775e720217c94b176d
Tyler Holt <tylerprestonholt@gmail.com>
2021-08-12 10:34:52 -0400

    create Header component

---
+++ client/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <header>
+        <h1>Streamroller</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
